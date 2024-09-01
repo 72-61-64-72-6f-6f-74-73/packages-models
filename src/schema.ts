ff3d8da7d048df388e5f7cab1f584ff34c2dd022
triesap <137732411+triesap@users.noreply.github.com>
2024-09-01 14:54:50 +0000

    Remove tab spacing

--- src/schema.ts
+++ src/schema.ts
@@ -3,8 +3,8 @@ import { regex } from "@radroots/utils";
 import type { IModelsForm, IModelsQueryBindValue, IModelsSortCreatedAt, IModelsFormErrorTuple, IModelsFormValidationTuple } from "./types";
 
 export const MassUnitSchema = z.union([
-	z.literal("g"),
-	z.literal("lb"),
+    z.literal("g"),
+    z.literal("lb"),
 ]);
 
 export type MassUnit = z.infer<typeof MassUnitSchema>;
@@ -12,7 +12,7 @@ export type MassUnit = z.infer<typeof MassUnitSchema>;
 export function parse_mass_unit(val: string): MassUnit {
     switch (val) {
         case "g":
-		case "lb":
+        case "lb":
             return val;
         default:
             return "g";
@@ -21,9 +21,9 @@ export function parse_mass_unit(val: string): MassUnit {
 
 export const LocationGcsSchema = z.object({
     lat: z.number({ message: "model.location_gcs.schema.lat.required" }).min(-90, { message: "model.location_gcs.schema.lat.min" }).max(90, { message: "model.location_gcs.schema.lat.max" }),
-	lng: z.number({ message: "model.location_gcs.schema.lng.required" }).min(-180, { message: "model.location_gcs.schema.lng.min" }).max(180, { message: "model.location_gcs.schema.lng.max" }),
-	geohash: z.string({ message: "model.location_gcs.schema.geohash.required" }),
-	label: z.string({ message: "model.location_gcs.schema.label.required" })
+    lng: z.number({ message: "model.location_gcs.schema.lng.required" }).min(-180, { message: "model.location_gcs.schema.lng.min" }).max(180, { message: "model.location_gcs.schema.lng.max" }),
+    geohash: z.string({ message: "model.location_gcs.schema.geohash.required" }),
+    label: z.string({ message: "model.location_gcs.schema.label.required" })
 });
 
 export type LocationGcsFields = z.infer<typeof LocationGcsSchema>;
@@ -67,17 +67,17 @@ export const location_gcs_form_fields: Record<keyof LocationGcsFormFields, IMode
         charset: regex.alpha,
         optional: false,
     },
-	lng: {
+    lng: {
         validation: regex.alpha,
         charset: regex.alpha,
         optional: false,
     },
-	geohash: {
+    geohash: {
         validation: regex.alpha,
         charset: regex.alpha,
         optional: false,
     },
-	label: {
+    label: {
         validation: regex.alphanum,
         charset: regex.alphanum,
         optional: false,
@@ -86,17 +86,17 @@ export const location_gcs_form_fields: Record<keyof LocationGcsFormFields, IMode
 
 export const location_gcs_form_vals: Record<keyof LocationGcsFormFields, string> = {
     lat: "",
-	lng: "",
-	geohash: "",
-	label: ""
+    lng: "",
+    geohash: "",
+    label: ""
 };
 
 export const parse_location_gcs_form_keys = (value: string): keyof LocationGcsFormFields | undefined => {
     switch (value) {
         case "lat":
-		case "lng":
-		case "geohash":
-		case "label":
+        case "lng":
+        case "geohash":
+        case "label":
             return value;
         default:
             return undefined;
@@ -106,31 +106,31 @@ export const parse_location_gcs_form_keys = (value: string): keyof LocationGcsFo
 export const parse_location_gcs_form_field_types = (value: string): "string" | "number" => {
     switch (value) {
         case "geohash":
-		case "label":
-			return "string";
-		case "lat":
-		case "lng":
-			return "number";
-		default:
+        case "label":
+            return "string";
+        case "lat":
+        case "lng":
+            return "number";
+        default:
             throw new Error("Error: parse_location_gcs_transform did not match.");
     };
 };
 
 export const location_gcs_sql = `CREATE TABLE IF NOT EXISTS location_gcs (
-	id CHAR(36) PRIMARY KEY NOT NULL UNIQUE CHECK(length(id) = 36),
+    id CHAR(36) PRIMARY KEY NOT NULL UNIQUE CHECK(length(id) = 36),
     created_at DATETIME NOT NULL CHECK(length(created_at) = 24),
     lat FLOAT NOT NULL,
-	lng FLOAT NOT NULL,
-	geohash CHAR(12) NOT NULL UNIQUE,
-	label TEXT,
-	CONSTRAINT unique_location_gcs UNIQUE (geohash)
+    lng FLOAT NOT NULL,
+    geohash CHAR(12) NOT NULL UNIQUE,
+    label TEXT,
+    CONSTRAINT unique_location_gcs UNIQUE (geohash)
 );`;
 
 export const TradeProductSchema = z.object({
     key: z.string({ message: "model.trade_product.schema.key.required" }),
-	lot: z.string({ message: "model.trade_product.schema.lot.required" }).min(1, { message: "model.trade_product.schema.lot.min" }).max(120, { message: "model.trade_product.schema.lot.max" }),
-	varietal: z.string({ message: "model.trade_product.schema.varietal.required" }),
-	notes: z.string().optional()
+    lot: z.string({ message: "model.trade_product.schema.lot.required" }).min(1, { message: "model.trade_product.schema.lot.min" }).max(120, { message: "model.trade_product.schema.lot.max" }),
+    varietal: z.string({ message: "model.trade_product.schema.varietal.required" }),
+    notes: z.string().optional()
 });
 
 export type TradeProductFields = z.infer<typeof TradeProductSchema>;
@@ -174,17 +174,17 @@ export const trade_product_form_fields: Record<keyof TradeProductFormFields, IMo
         charset: regex.alpha,
         optional: false,
     },
-	lot: {
+    lot: {
         validation: regex.alphanum,
         charset: regex.alphanum,
         optional: false,
     },
-	varietal: {
+    varietal: {
         validation: regex.alphanum,
         charset: regex.alphanum,
         optional: false,
     },
-	notes: {
+    notes: {
         validation: regex.alpha,
         charset: regex.alpha,
         optional: true,
@@ -193,17 +193,17 @@ export const trade_product_form_fields: Record<keyof TradeProductFormFields, IMo
 
 export const trade_product_form_vals: Record<keyof TradeProductFormFields, string> = {
     key: "",
-	lot: "",
-	varietal: "",
-	notes: ""
+    lot: "",
+    varietal: "",
+    notes: ""
 };
 
 export const parse_trade_product_form_keys = (value: string): keyof TradeProductFormFields | undefined => {
     switch (value) {
         case "key":
-		case "lot":
-		case "varietal":
-		case "notes":
+        case "lot":
+        case "varietal":
+        case "notes":
             return value;
         default:
             return undefined;
@@ -213,30 +213,30 @@ export const parse_trade_product_form_keys = (value: string): keyof TradeProduct
 export const parse_trade_product_form_field_types = (value: string): "string" | "number" => {
     switch (value) {
         case "key":
-		case "lot":
-		case "varietal":
-		case "notes":
-			return "string";
-		default:
+        case "lot":
+        case "varietal":
+        case "notes":
+            return "string";
+        default:
             throw new Error("Error: parse_trade_product_transform did not match.");
     };
 };
 
 export const trade_product_sql = `CREATE TABLE IF NOT EXISTS trade_product (
-	id CHAR(36) PRIMARY KEY NOT NULL UNIQUE CHECK(length(id) = 36),
+    id CHAR(36) PRIMARY KEY NOT NULL UNIQUE CHECK(length(id) = 36),
     created_at DATETIME NOT NULL CHECK(length(created_at) = 24),
     key TEXT,
-	lot TEXT,
-	varietal TEXT,
-	notes TEXT,
-	CONSTRAINT unique_trade_product UNIQUE (key, lot, varietal)
+    lot TEXT,
+    varietal TEXT,
+    notes TEXT,
+    CONSTRAINT unique_trade_product UNIQUE (key, lot, varietal)
 );`;
 
 export const NostrNoteSchema = z.object({
     ev_id: z.string({ message: "model.nostr_note.schema.ev_id.required" }),
-	ev_created_at: z.number({ message: "model.nostr_note.schema.ev_created_at.required" }),
-	ev_content: z.string({ message: "model.nostr_note.schema.ev_content.required" }),
-	ev_tags: z.string({ message: "model.nostr_note.schema.ev_tags.required" })
+    ev_created_at: z.number({ message: "model.nostr_note.schema.ev_created_at.required" }),
+    ev_content: z.string({ message: "model.nostr_note.schema.ev_content.required" }),
+    ev_tags: z.string({ message: "model.nostr_note.schema.ev_tags.required" })
 });
 
 export type NostrNoteFields = z.infer<typeof NostrNoteSchema>;
@@ -280,17 +280,17 @@ export const nostr_note_form_fields: Record<keyof NostrNoteFormFields, IModelsFo
         charset: regex.alpha,
         optional: false,
     },
-	ev_created_at: {
+    ev_created_at: {
         validation: regex.alpha,
         charset: regex.alpha,
         optional: false,
     },
-	ev_content: {
+    ev_content: {
         validation: regex.alpha,
         charset: regex.alpha,
         optional: false,
     },
-	ev_tags: {
+    ev_tags: {
         validation: regex.alpha,
         charset: regex.alpha,
         optional: false,
@@ -299,17 +299,17 @@ export const nostr_note_form_fields: Record<keyof NostrNoteFormFields, IModelsFo
 
 export const nostr_note_form_vals: Record<keyof NostrNoteFormFields, string> = {
     ev_id: "",
-	ev_created_at: "",
-	ev_content: "",
-	ev_tags: ""
+    ev_created_at: "",
+    ev_content: "",
+    ev_tags: ""
 };
 
 export const parse_nostr_note_form_keys = (value: string): keyof NostrNoteFormFields | undefined => {
     switch (value) {
         case "ev_id":
-		case "ev_created_at":
-		case "ev_content":
-		case "ev_tags":
+        case "ev_created_at":
+        case "ev_content":
+        case "ev_tags":
             return value;
         default:
             return undefined;
@@ -319,38 +319,38 @@ export const parse_nostr_note_form_keys = (value: string): keyof NostrNoteFormFi
 export const parse_nostr_note_form_field_types = (value: string): "string" | "number" => {
     switch (value) {
         case "ev_id":
-		case "ev_content":
-		case "ev_tags":
-			return "string";
-		case "ev_created_at":
-			return "number";
-		default:
+        case "ev_content":
+        case "ev_tags":
+            return "string";
+        case "ev_created_at":
+            return "number";
+        default:
             throw new Error("Error: parse_nostr_note_transform did not match.");
     };
 };
 
 export const nostr_note_sql = `CREATE TABLE IF NOT EXISTS nostr_note (
-	id CHAR(36) PRIMARY KEY NOT NULL UNIQUE CHECK(length(id) = 36),
+    id CHAR(36) PRIMARY KEY NOT NULL UNIQUE CHECK(length(id) = 36),
     created_at DATETIME NOT NULL CHECK(length(created_at) = 24),
     ev_id TEXT,
-	ev_created_at REAL,
-	ev_content TEXT,
-	ev_tags TEXT,
-	CONSTRAINT unique_nostr_note UNIQUE (ev_id)
+    ev_created_at REAL,
+    ev_content TEXT,
+    ev_tags TEXT,
+    CONSTRAINT unique_nostr_note UNIQUE (ev_id)
 );`;
 
 export const trade_product_location_sql = `CREATE TABLE IF NOT EXISTS trade_product_location (
     tb_tploc_0 CHAR(36),
-	tb_tploc_1 CHAR(36),
+    tb_tploc_1 CHAR(36),
     FOREIGN KEY (tb_tploc_0) REFERENCES trade_product(id) ON DELETE CASCADE,
-	FOREIGN KEY (tb_tploc_1) REFERENCES location_gcs(id) ON DELETE CASCADE,
+    FOREIGN KEY (tb_tploc_1) REFERENCES location_gcs(id) ON DELETE CASCADE,
     PRIMARY KEY (tb_tploc_0, tb_tploc_1)
 );`;
 
 export const models_initial_upgrade = [
-	`PRAGMA foreign_keys = ON;`,
-	location_gcs_sql,
-	trade_product_sql,
-	nostr_note_sql,
-	trade_product_location_sql
+    `PRAGMA foreign_keys = ON;`,
+    location_gcs_sql,
+    trade_product_sql,
+    nostr_note_sql,
+    trade_product_location_sql
 ];
